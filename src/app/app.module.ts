5f8c9853f59de088477473503a366f154873929d
bobnunz <bobnunz@yahoo.com>
2021-01-30 10:49:55 -0500

    demo screen for napsc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RrMainPageComponent } from './components/rr-main-page/rr-main-page.component';
+
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { RrMainPageComponent } from './components/rr-main-page/rr-main-page.comp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
